5595e7e149ffc0c09625afd918016937cd4f80ad
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2023-07-26 13:27:43 +0100

    Added Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/nicole-baylis/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nicole Baylis
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/NicoleBay/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nicole-baylis/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nicole Baylis
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/NicoleBay/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
