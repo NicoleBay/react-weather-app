6c55d48e5981da0f15ca33b713e18a31e798e8ee
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2023-07-26 13:01:42 +0100

    Added footer with LinkedIn profile URL and GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/nicole-baylis/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nicole Baylis
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/NicoleBay/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
