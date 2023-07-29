e66016d735625a45c18cebc3bab919d75a0a1770
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2023-07-29 17:13:19 +0100

    Added unit conversion to current temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,10 +17,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-2 temperature-now">
         <div className="col-6">
           <img src={props.data.iconUrl} alt={props.data.description} />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
 
         <div className="col-6 list">
